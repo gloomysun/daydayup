6363caa8a82f5cc646ca9bd403d86e9d6751e59e
ly <409575046@qq.com>
2018-11-05 17:33:07 +0800

    springboot+rabbitmq 消息确认

---
+++ springboot/rabbitmq/mqconfirm/src/main/java/com/ly/mqconfirm/MqconfirmApplication.java
@@ -0,0 +1,12 @@
+package com.ly.mqconfirm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqconfirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqconfirmApplication.class, args);
+	}
+}
---
+++ springboot/rabbitmq/mqconfirm/src/main/java/com/ly/mqconfirm/config/RabbitmqConfig.java
@@ -0,0 +1,9 @@
+package com.ly.mqconfirm.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+
+}
